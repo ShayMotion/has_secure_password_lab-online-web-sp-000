b6acc6e5218119c4dcfd5c6b80eb51d9525d300b
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-03-05 18:00:04 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   def create
     # binding.pry
     @user = User.find_by(name: params[:user][:name])#.try(:authenticate, params["user"]["password"])
+    
     # return head(:forbidden) unless @user.authenticate(params[:password])
     # @user = User.find_by(name: params[:user][:username])
     if @user == nil
